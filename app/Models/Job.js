808e08f07658b285d2648bc64c7a3119bd1a948c
JustinBarnard1 <jbarnard0031@gmail.com>
2020-08-26 15:04:36 -0600

    Some Styling Added

--- app/Models/Job.js
+++ app/Models/Job.js
@@ -19,7 +19,7 @@ export default class Job {
                 <div class="d-flex justify-content-between">
                     <button class="btn btn-outline-danger"
                         onclick="app.jobsController.removeJob('${this.id}')">Delete</button>
-                    <button class="btn btn-outline-info" onclick="alert('You Wish! You are completely unqualified for this job! How about you go back to school and do something with your life! Flippin loser.')">Apply Now</button>
+                    <button class="btn btn-outline-info" onclick="alert('You Wish! You are completely unqualified for this job! What are you even thinking? Stick to your entry level job if you think you are ready for this! How about you go back to school and do something with your life! Maybe try contributing to society? If you really wanted to make a change and do something new you would go to BoiseCodeWorks and start coding! Are you still reading this? Why are you still reading this? Stop procrasinating and get to it slacker! You are STILL reading this? On second thought, you are never going to make it if you waste this much time just reading this dumb prompt. Also, you should know... Have a great day!')">Apply Now</button>
                 </div>
             </div>
                     </div>
